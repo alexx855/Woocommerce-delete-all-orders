7744e0efe4558952802ca997ca9bf8ee8381a444
Alex Pedersen <alexpedersen1993@gmail.com>
2020-07-13 11:56:58 +0200

    Update wooDelete.php

--- wooDelete.php
+++ wooDelete.php
@@ -1,14 +1,4 @@
 <?php
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <mauran@mauran.me> wrote this file.
- * As long as you retain this notice you can do whatever you want with this
- * stuff. If we meet some day, and you think this stuff is worth it, you can
- * buy me a beer in return --Mauran Muthiah
- * ----------------------------------------------------------------------------
- */
-
 // By default wp_ for standard WP-installations
 $prefix = 'wp_';
 
@@ -24,44 +14,12 @@ if ($conn->connect_error) {
     die('Whoops: '.$conn->connect_error);
 }
 
-$sql = 'DELETE FROM '.$prefix.'woocommerce_order_itemmeta';
+$sql = 'DELETE wu FROM `wp_users` wu INNER JOIN '.$prefix.'_usermeta ON wu.ID = '.$prefix.'_usermeta.user_id WHERE meta_key = 'wp_capabilities' AND meta_value NOT LIKE '%administrator%'';
 
 if ($conn->query($sql) === true) {
-    echo 'Deleted woocommerce_order_itemmeta '.PHP_EOL;
+    echo 'Users deleted. '.PHP_EOL;
 } else {
     echo 'Whoops: '.$conn->error;
 }
 
-$sql = 'DELETE FROM '.$prefix.'woocommerce_order_items';
-
-if ($conn->query($sql) === true) {
-    echo 'Deleted woocommerce_order_items'.PHP_EOL;
-} else {
-    echo 'Whoops: '.$conn->error;
-}
-
-$sql = 'DELETE FROM '.$prefix."comments WHERE comment_type = 'order_note'";
-
-if ($conn->query($sql) === true) {
-    echo "Deleted comments where comment_type = 'order_note'".PHP_EOL;
-} else {
-    echo 'Whoops: '.$conn->error;
-}
-
-$sql = 'DELETE FROM '.$prefix.'postmeta WHERE post_id IN ( SELECT ID FROM '.$prefix."posts WHERE post_type = 'shop_order' )";
-
-if ($conn->query($sql) === true) {
-    echo "Deleted all rows from postmeta which belonged to posts with post_type = 'shop_order'".PHP_EOL;
-} else {
-    echo 'Whoops: '.$conn->error;
-}
-
-$sql = 'DELETE FROM '.$prefix."posts WHERE post_type = 'shop_order'";
-
-if ($conn->query($sql) === true) {
-    echo "Deleted posts where post_type = 'shop_order'".PHP_EOL;
-} else {
-    echo 'Whoops: '.$conn->error;
-}
-
-echo 'Fixed! Your woocommerce orders should be empty! :-)';
+echo 'Fixed! Your Wordpress users should be empty! :-)';
